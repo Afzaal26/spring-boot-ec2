c946117208fd20e02a2352d8b4847d2ed0fd67d3
Afzaal Hussain <92880188+Afzaal26@users.noreply.github.com>
2025-08-21 23:40:22 +0500

    Delete CarRentalSystem directory

--- CarRentalSystem/CarRentalSystem/src/main/java/CarRentalSystem/repository/TestingRepository.java
+++
@@ -1,9 +0,0 @@
-package CarRentalSystem.repository;
-
-import CarRentalSystem.entity.TestingTable;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TestingRepository extends JpaRepository<TestingTable, Long> {
-}
